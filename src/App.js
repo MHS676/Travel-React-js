a0e27d2eb482976d55fc8ccf621675a8e6f951e8
Hasan Talukder <mahadihasansiam2@gmail.com>
2023-09-14 18:19:01 +0600

    Submodule update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>App</h2>
+      {/* You can include your logo here if needed */}
+      {/* <img src={logo} alt="Logo" /> */}
     </div>
   );
 }
 
 export default App;
+
